8f697cf8e31eb01b0c7eebfb16489eaa7c7f4eeb
Magnus Mølgaard Christensen <magnuschristensen42@gmail.com>
2023-12-19 11:12:55 +0100

    fized title on row

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -7,7 +7,7 @@ import {Grid} from "@mui/material";
 
 export const Frontpage = () => {
   return (
-    <Grid container justifyContent="center" alignItems="center" >
+    <Grid container direction="column"  justifyContent="center" alignItems="center" >
         <h1 style={{ color: 'black' }}>Weather App</h1>
         <WeatherComponent />
     </Grid>
